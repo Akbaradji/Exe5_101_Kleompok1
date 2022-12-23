876d638303e1558ee9be2eceff0a2a648c75a809
Akbar Adji Herlambang <akbaradjiherlambang@gmail.com>
2022-12-23 03:55:32 -1200

    add class queue

--- Program.cs
+++ Program.cs
@@ -7,6 +7,15 @@ namespace Exe5_101_Kelompok1
         public string name;
         public Node next;
     }
+    class queue
+    {
+        Node front, rear;
+        public queue()
+        {
+            front = null;
+            rear = null;
+        }
+    }
 }
 
 
